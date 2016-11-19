14eecd5bbfa2a723a12e572aecb09cbbeffb9842
kzts <tanaka@isi.imi.i.u-tokyo.ac.jp>
2016-11-19 18:17:52 +0900

    send motion

--- src/remoteControlPichingMachine.c
+++ src/remoteControlPichingMachine.c
@@ -7,8 +7,18 @@
 #include <unistd.h>
 #define PITCHING_MACHINE_IP "192.168.2.20"
 
+//#define NUM_OF_BUFFER 255
+//#define NUM_OF_BUFFER 99999
+#define NUM_OF_BUFFER 1024
+//char buffer[1024];
+//char buffer[99999];
+//char buffer[NUM_OF_BUFFER];
+char buffer_recv[NUM_OF_BUFFER];
+char buffer_send[NUM_OF_BUFFER];
+
 // bbb
 #include <stdlib.h>
+#include <time.h>
 #define MAX_str 1024
 #define NUM_OF_SAMPLES 99999
 #define NUM_OF_PHASE 10
@@ -24,6 +34,8 @@ unsigned long data_sensors[NUM_OF_SAMPLES][NUM_ADC][NUM_ADC_PORT] = {};
 double time_switch[NUM_OF_PHASE] = {};
 double value_valves[NUM_OF_CHANNELS] = {};
 double value_valves_phase[NUM_OF_PHASE][NUM_OF_CHANNELS] = {};
+
+struct timeval ini_t, now_t;
 /*
 unsigned long Value_sensors[NUM_ADC][NUM_ADC_PORT] = {};
 double Time_phase[NUM_OF_PHASE] = {};
@@ -34,7 +46,7 @@ void saveResults(void){
   FILE *fp_res;
   char str[MAX_str];
 
-  fp_res = fopen( "data/results.dat", "w");
+  fp_res = fopen( "../data/results.dat", "w");
 
   if (fp_res == NULL){
     printf("File open error (results)\n");
@@ -121,7 +133,7 @@ void loadCommand(void){
       value_valves_phase[i][j] = atof( s[j+2]);
     */
   }
-
+  
   for (i = 0; i < NUM_OF_PHASE; i++){
     sum_time = 0.0;
     for (j = 0; j <= i; j++)
@@ -146,7 +158,60 @@ void loadCommand(void){
   fclose(fp_cmd);
 }
 
+void setCommandBuffer(int phase){
+  int j;
+  char tmp[9];
+  //printf( "%s ", buffer_send );
 
+  for ( j = 0; j < NUM_OF_CHANNELS; j++ ){
+    sprintf( tmp, "%4.3f ", value_valves_phase[phase][j] );
+    strcat( buffer_send, tmp );
+    //printf( "%s ", buffer_send );
+  }
+  
+  //printf("\n");
+}
+
+double getTime(int i){
+  double now_time_;
+  gettimeofday( &now_t, NULL );
+  now_time_ = 
+    + 1000.0*( now_t.tv_sec - ini_t.tv_sec ) 
+    + 0.001*( now_t.tv_usec - ini_t.tv_usec );
+
+  data_time[i] = now_time_;
+  return now_time_;
+}
+
+void setSensorValue(int i){
+  int j, k;
+  char *tmp;
+
+  tmp = strtok( buffer_recv, " " ); 
+  for (j = 0; j< NUM_ADC; j++){
+    for (k = 0; k< NUM_ADC_PORT; k++){
+      tmp = strtok( NULL, " " ); 
+      data_sensors[i][j][k] = atof( tmp );
+    }
+  }
+}
+
+void setValveValue(int i, int phase){
+  int j;
+  for ( j = 0; j < NUM_OF_CHANNELS; j++ )
+    data_valves[i][j] = value_valves_phase[phase][j];
+}
+
+int getPhaseNumber( double elasped_t ){
+  unsigned int i;
+  unsigned int phase = 0;
+
+  for ( i = 0; i < NUM_OF_PHASE; i++)
+      if ( elasped_t > time_switch[i] )
+        phase = i + 1;
+
+  return phase;
+}
 
 int main(int argc, char* argv[]){
 
@@ -160,8 +225,6 @@ int main(int argc, char* argv[]){
 
   // socket 
   int clientSocket;
-  //char buffer[1024];
-  char buffer[99999];
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
 
@@ -176,21 +239,50 @@ int main(int argc, char* argv[]){
   connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
 
   // loop
-  loadCommand();
-  strcpy( buffer, "no" );
+  int now_phase = 0, old_phase = -1;
+  double now_time;
 
-  int i = 0;
-  while ( strcmp( buffer, "END" ) != 0 ){
-    recv( clientSocket, buffer, 1024, 0);
+  loadCommand();
+  gettimeofday( &ini_t, NULL );
+ 
+  //strcpy( buffer, "no" );
+  int i;
+  //while ( strcmp( buffer, "END" ) != 0 ){
+  for ( i = 0; i < NUM_OF_SAMPLES; i++ ){
+    now_time = getTime(i);
+    now_phase = getPhaseNumber( now_time );
+    //printf( "%d \t %d \t %8.3f \n", i, now_phase, now_time );
+
+    if ( now_phase >= NUM_OF_PHASE )
+      break;
+
+    recv( clientSocket, buffer_recv, NUM_OF_BUFFER, 0);
     
-    if ( strcmp( buffer, "no" ) != 0 ){
-      printf("%s \n", buffer);
+    //if ( strcmp( buffer, "no" ) != 0 ){
+    //printf("%s \n", buffer);
+    if ( strlen( buffer_recv ) > strlen( "sensor: " ))
+      setSensorValue(i);
+    setValveValue(i,now_phase);
+    //}
+
+    if ( now_phase != old_phase ){
+      printf("now phase: %d\n", now_phase );
+      strcpy( buffer_send, "command: " ); 
+      //strcpy( buffer_send, "command: 0.00 0.450 0.333 0  0 0 0 0  0 0 0 0  0 0 0 0 ");
+      //printf("buffer in main: %s\n", buffer_send );
+      setCommandBuffer( now_phase );
+    }else{
+      strcpy( buffer_send, "no" );
     }
-    if ( i == 10 )
-      send( clientSocket, "command: 0.00 0.450 0.333 0  0 0 0 0  0 0 0 0  0 0 0 0 ", 99, 0);
-    else
-      send( clientSocket, "no", 13, 0);
-    i++;
+    //send( clientSocket, buffer, 1024, 0);
+    send( clientSocket, buffer_send, NUM_OF_BUFFER, 0 );
+
+    //if ( i == 10 )
+    //send( clientSocket, "command: 0.00 0.450 0.333 0  0 0 0 0  0 0 0 0  0 0 0 0 ", 99, 0);
+    //else
+    //
+    //i++;
+    old_phase = now_phase;
   }
   saveResults();
   return 0;
