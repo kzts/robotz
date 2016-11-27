19ec0e007f929261368f5bc296cf3b0404ae1750
kzts <tanaka@isi.imi.i.u-tokyo.ac.jp>
2016-11-27 21:00:17 +0900

    get ip from file

--- src/remoteControlPitchingMachine.c
+++ src/remoteControlPitchingMachine.c
@@ -34,6 +34,8 @@ double time_switch[NUM_OF_PHASE] = {};
 double value_valves[NUM_OF_CHANNELS] = {};
 double value_valves_phase[NUM_OF_PHASE][NUM_OF_CHANNELS] = {};
 
+char filename_ip_pitching[] = "../data/params/ip_pitching.dat";
+
 struct timeval ini_t, now_t;
 /*
 unsigned long Value_sensors[NUM_ADC][NUM_ADC_PORT] = {};
@@ -212,16 +214,53 @@ int getPhaseNumber( double elasped_t ){
   return phase;
 }
 
+int connectSocket( char* ip_address, int port_num ){
+  int clientSocket;
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+  // Create the socket. The three arguments are:
+  // 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) 
+  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  //---- Configure settings of the server address struct
+  serverAddr.sin_family = AF_INET; // Address family = Internet
+  serverAddr.sin_port = htons(port_num);   // Set port number, using htons function to use proper byte order
+  serverAddr.sin_addr.s_addr = inet_addr(ip_address); // Set IP address to localhost 
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero); // Set all bits of the padding field to 0 
+  // Connect the socket to the server using the address struct
+  addr_size = sizeof serverAddr;
+  connect( clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+
+  return clientSocket;
+}
+
+void getIPAddress( char* filename, char* ip_address ){
+  FILE *fp;
+  fp = fopen( filename, "r" );
+  if ( fp == NULL ){
+    printf( "File open error: %s\n", filename );
+    return;
+  }
+  fgets( ip_address, NUM_OF_BUFFER, fp );
+  fclose(fp);
+}
+
 int main(int argc, char* argv[])
 {
+  char ip_pitching[NUM_OF_BUFFER];
+  getIPAddress( filename_ip_pitching,  ip_pitching );   
+  printf( "ip address: %s (pitching)\n", ip_pitching );
+  int port_num = 7891;
+  int clientSocket  = connectSocket( ip_pitching,  port_num );
+  /*
   if ( argc != 2 ){
     printf("input Pitching macine's ip address.\n");
     return -1;
   }
   char* ip_address = argv[1];
-
+  */
 
   // socket 
+  /*
   int clientSocket;
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
@@ -234,6 +273,7 @@ int main(int argc, char* argv[])
 
   addr_size = sizeof serverAddr;
   connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+  */
 
   // loop
   int now_phase = 0, old_phase = -1;
